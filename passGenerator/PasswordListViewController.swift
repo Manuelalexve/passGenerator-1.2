78283806bea07d69be28980ab5b70b88309bd03c
Manuel Dominguez <manuelalexve@gmail.com>
2024-08-07 10:05:05 -0400

    se agrego una nueva clase con una nueva vista para la tabla de paswords

---
+++ passGenerator/PasswordListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  viewcontroller2.swift
+//  passGenerator
+//
+//  Created by manuel on 8/7/24.
+//
+
+import UIKit
+import CoreData
+
+class PasswordListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Contexto de Core Data
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var passwords: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Configura el Table View
+        tableView.dataSource = self
+        tableView.delegate = self
+        loadPasswords()
+    }
+    
+    func loadPasswords() {
+        // Carga las contraseñas guardadas de Core Data
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Password")
+        do {
+            let passwords = try context.fetch(fetchRequest) as! [NSManagedObject]
+            self.passwords = passwords
+            tableView.reloadData()
+        } catch {
+            print("Error al cargar contraseñas: \(error)")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return passwords.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "passwordCell", for: indexPath)
+        let password = passwords[indexPath.row]
+        cell.textLabel?.text = password.value(forKey: "password") as? String
+        return cell
+    }
+}
+
